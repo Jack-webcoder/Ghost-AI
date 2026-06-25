442397ca4a97eae82e5a75856aaba8c2b8572c02
Jack-webcoder <Jack-webcoder@users.noreply.github.com>
2026-06-25 21:29:21 +0800

    Save current project updates

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "@trigger.dev/sdk";
 
 export default defineConfig({
   project: "proj_wgwljrxihsvhmvudimzz", // Replace with your project ID from https://dashboard.trigger.dev
-  dirs: ["./trigger"],
+  runtime:"node",
+  dirs: ["trigger"],
   maxDuration: 300,
+  retries:{
+    enabledInDev: false,
+    default:{
+      maxAttempts: 3,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 10000,
+      factor: 2,
+    }
+  }
 });
